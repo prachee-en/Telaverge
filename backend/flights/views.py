a1c78bfec5abaa4a512addb9cb873bed4eca6d96
prachee-en <prachiagarwal2002july@gmail.com>
2024-09-23 01:44:14 +0530

    added search filters to get flight api

--- backend/flights/views.py
+++ backend/flights/views.py
@@ -3,9 +3,12 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Flight
 from .serializers import FlightSerializer
+from django.http import HttpResponseBadRequest
 
 @api_view(['GET'])
-def flight_list(request):
-    flights = Flight.objects.all()
+def flight_list(request, from_city, to_city, date):
+    if not from_city or not to_city or not date:
+        return HttpResponseBadRequest("Missing required parameters")
+    flights = Flight.objects.filter(departure_city=from_city, arrival_city=to_city, departure_time__date=date)
     serializer = FlightSerializer(flights, many=True)
     return Response(serializer.data)
