dc4d05d73ed56a065257222642dbd0c29e3893e1
Lakshya Agarwal <lakshyaagarwal34@gmail.com>
2024-09-23 01:10:19 +0530

    done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import Buses from "./components/Buses/Buses";
 import FlightsTickets from "./components/FlightTickets/FlightsTickets";
+import BusTickets from "./components/BusTickets/BusTickets";
 import Footer from "./components/Footer";
 import Home from "./components/Home/Home";
-import Services from "./components/Home/Services";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -14,8 +14,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/flights" element={<Home />} />
-          <Route path="/flights/Tickets" element={<FlightsTickets/>} />
+          <Route path="/flights/tickets" element={<FlightsTickets />} />
           <Route path="/buses" element={<Buses />} />
+          <Route path="/buses/tickets" element={<BusTickets />} />
         </Routes>
         <Footer />
       </main>
--- frontend/src/components/Buses/Info.jsx
+++ frontend/src/components/Buses/Info.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Services from "../Home/Services";
+import BusSearch from "./BusSearch";
 
 const Info = () => {
   return (
     <>
       <Services />
-      <div className="w-5/6 h-3/5 bg-gray-200 mx-auto flex justify-between rounded-md">
-        <div className="text-black p-12 font-thin">
-          <h3>Book Buses</h3>
+      <div className="w-5/6 mx-auto bg-gray-200   justify-between items-center rounded-md p-8">
+        <div className="w-5/6 mx-auto mt-6">
+          <BusSearch />
         </div>
       </div>
     </>
