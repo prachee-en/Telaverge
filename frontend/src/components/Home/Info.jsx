62ce5a0403dd97c23691ea119832fa9efd09c758
Lakshya Agarwal <lakshyaagarwal34@gmail.com>
2024-09-22 22:18:13 +0530

    hi

--- frontend/src/components/Home/Info.jsx
+++ frontend/src/components/Home/Info.jsx
@@ -6,26 +6,22 @@ const Info = ({ flights }) => {
   return (
     <>
       <Services />
-      <div className="w-5/6 h-3/5 bg-gray-200 mx-auto flex justify-between rounded-md">
-        <div className="flex">
-          <div className="flex">
-            <div className="p-12 text-black flex gap-3">
-              <div className=" flex gap-1">
-                <input type="radio" name="tripType" />
-                <span>One Way</span>
-              </div>
-              <div className=" flex gap-1">
-                <input type="radio" name="tripType" />
-                <span>Round Trip</span>
-              </div>
-            </div>
+      <div className="w-5/6 h-3/5 bg-gray-200 flex mx-auto justify-between rounded-md">
+        <div className="p-12 text-black flex gap-3">
+          <div className=" flex gap-1">
+            <input type="radio" name="tripType" />
+            <span>One Way</span>
           </div>
-          <div className="text-black p-12 font-thin">
-            <h3>Book International and Domestic Flights</h3>
+          <div className=" flex gap-1">
+            <input type="radio" name="tripType" />
+            <span>Round Trip</span>
           </div>
         </div>
-        <FlightSearch />
+        <div className="text-black p-12 font-thin">
+          <h3>Book International and Domestic Flights</h3>
+        </div>
       </div>
+      <FlightSearch />
     </>
   );
 };
