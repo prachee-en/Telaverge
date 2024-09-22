677b1f865462242f2187d1cfd68d8c639bb7a602
Lakshya Agarwal <lakshyaagarwal34@gmail.com>
2024-09-22 19:04:19 +0530

    api

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,7 +1,29 @@
-import React from "react";
 import Info from "./Info";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Home = () => {
+  const [flights, setFlights] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    const fetchFlights = async () => {
+      try {
+        const response = await axios.get(
+          "https://api.aviationstack.com/v1/flights?access_key=bf0e6fbd791c4670eccd6e05634dc9ab"
+        );
+
+        setFlights(response.data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchFlights();
+  }, []);
+  console.log(flights);
   return (
     <div
       className="top w-full h-[70vh]  bg-cover bg-center"
@@ -10,7 +32,7 @@ const Home = () => {
           "url('https://media.istockphoto.com/id/1341288649/photo/75mpix-panorama-of-beautiful-mount-ama-dablam-in-himalayas-nepal.jpg?s=1024x1024&w=is&k=20&c=pxNX4Ns19juNzCiJInjnjnWjzugXnZBLblSge_rNKLs=')",
       }}
     >
-      <Info />
+      <Info flights={flights} />
     </div>
   );
 };
--- frontend/src/components/Home/Info.jsx
+++ frontend/src/components/Home/Info.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Services from "./Services";
 
-const Info = () => {
+const Info = ({ flights }) => {
   return (
     <>
       <Services />
